06f0c13ec68d283c33f695ab6a7e4526e348df0b
tomsza26 <tomsza27@gmail.com>
2020-01-23 16:32:13 +0100

    drugi commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './App.scss';
+import Clock from './components/Clock';
 
-function App() {
-	return <div className="App" />;
+class App extends React.Component {
+	render() {
+		return (
+			<div className="App">
+				<Clock />
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/Clock.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import '../App.scss';
+import { ReactComponent as Reset } from './reset.svg';
+import { ReactComponent as Play_pause } from './play_pause.svg';
+import { ReactComponent as Up_arrow } from './up-arrow.svg';
+import { ReactComponent as Down_arrow } from './down-arrow.svg';
+
+class Clock extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			breakT: 5,
+			sessionT: 25
+		};
+	}
+
+	render() {
+		return (
+			<div class="flex">
+				<header id="pomodoro-name">pomodoro clock</header>
+				<section className="break-session">
+					<article className="bcontrol">
+						<header id="break-label">break length</header>
+						<button id="break-decrement">
+							<Down_arrow />
+						</button>
+						<div id="break-length">{this.state.breakT}</div>
+						<button id="break-increment">
+							<Up_arrow />
+						</button>
+					</article>
+					<article className="scontrol">
+						<header id="session-label">session length</header>
+						<button id="session-decrement">
+							<Down_arrow />
+						</button>
+						<div id="session-length">{this.state.sessionT}</div>
+						<button id="session-increment">
+							<Up_arrow />
+						</button>
+					</article>
+				</section>
+				<section className="session-container">
+					<header id="timer-label">session</header>
+					<div id="time-left">{this.state.sessionT}</div>
+					<button id="start_stop">
+						<Play_pause />
+					</button>
+					<button id="reset">
+						<Reset />
+					</button>
+				</section>
+			</div>
+		);
+	}
+}
+
+export default Clock;
