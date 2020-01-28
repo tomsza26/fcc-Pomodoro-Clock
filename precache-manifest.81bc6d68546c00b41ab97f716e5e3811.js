c03d1c8f5e4471366891f895c4e3e08acd25affe
tomsza26 <tomsza27@gmail.com>
2020-01-28 01:07:31 +0100

    Updates

---
+++ precache-manifest.81bc6d68546c00b41ab97f716e5e3811.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a683d06426d61c361829bdd251a4a92",
+    "url": "/fcc-Pomodoro-Clock/index.html"
+  },
+  {
+    "revision": "066f707737c591119404",
+    "url": "/fcc-Pomodoro-Clock/static/css/2.09ae8418.chunk.css"
+  },
+  {
+    "revision": "6a68a533fe02dbdd4721",
+    "url": "/fcc-Pomodoro-Clock/static/css/main.7d3339e7.chunk.css"
+  },
+  {
+    "revision": "066f707737c591119404",
+    "url": "/fcc-Pomodoro-Clock/static/js/2.f3854714.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/fcc-Pomodoro-Clock/static/js/2.f3854714.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6a68a533fe02dbdd4721",
+    "url": "/fcc-Pomodoro-Clock/static/js/main.6642dd59.chunk.js"
+  },
+  {
+    "revision": "c686a956ac02040052e5",
+    "url": "/fcc-Pomodoro-Clock/static/js/runtime-main.87c6b284.js"
+  },
+  {
+    "revision": "6f2dc5e9eb904cef4ccc31b77fc69f56",
+    "url": "/fcc-Pomodoro-Clock/static/media/down-arrow.6f2dc5e9.svg"
+  },
+  {
+    "revision": "2e0aa0b6fefd84c26ef0b760c572fcf4",
+    "url": "/fcc-Pomodoro-Clock/static/media/leaf.2e0aa0b6.svg"
+  },
+  {
+    "revision": "f428ce33c3cd25263e50a09b556bc3f0",
+    "url": "/fcc-Pomodoro-Clock/static/media/next.f428ce33.svg"
+  },
+  {
+    "revision": "be85d77fcd82f92f409d97f4644d3fc8",
+    "url": "/fcc-Pomodoro-Clock/static/media/play_pause.be85d77f.svg"
+  },
+  {
+    "revision": "35bd2daed45cf2e9c17966e33606744d",
+    "url": "/fcc-Pomodoro-Clock/static/media/reset.35bd2dae.svg"
+  },
+  {
+    "revision": "85d215f65acf9fd4a137801b8fd80cc1",
+    "url": "/fcc-Pomodoro-Clock/static/media/up-arrow.85d215f6.svg"
+  }
+]);
